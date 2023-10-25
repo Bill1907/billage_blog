59de692abe58cec40d93ed8fa3c44f8ef95e9201
Bill1907 <a31829660@gamil.com>
2023-10-25 23:43:56 +0900

    apply toast ui

--- app/blog/create/page.tsx
+++ app/blog/create/page.tsx
@@ -1,37 +1,45 @@
-'use client';
-import "@uiw/react-md-editor/markdown-editor.css";
-import "@uiw/react-markdown-preview/markdown.css";
-import dynamic from "next/dynamic";
-import { useState } from "react";
-import Header from "@/components/Header";
+'use client'
+import dynamic from 'next/dynamic'
+import React, { useRef, useState } from 'react'
+import Header from '@/components/Header'
+import '@toast-ui/editor/dist/toastui-editor.css'
 
-const MDEditor = dynamic(
-  () => import("@uiw/react-md-editor"),
-  { ssr: false }
-);
+import { Editor } from '@toast-ui/react-editor'
 
 export default function BlogCreatePage() {
-  const [value, setValue] = useState("**Hello world!!!** \n\n ```js \n console.log('Hello world!!!') \n ```");
+  const editorRef = useRef<Editor>(null)
+  const [value, setValue] = useState(
+    "**Hello world!!!** \n\n ```js \n console.log('Hello world!!!') \n ```",
+  )
   const handleClickSaveBlog = async () => {
-      const result = await fetch("http://localhost:3000/api/blog", {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify({content: value})
-      }).then(res => res.json())
-      console.log('save blog', result)
+    const editorMarkdown = editorRef.current?.getInstance().getMarkdown()
+    console.log(editorMarkdown)
+    // const result = await fetch('http://localhost:3000/api/blog', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({ content: value }),
+    // }).then((res) => res.json())
+    // console.log('save blog', result)
   }
   return (
     <main>
-        <section data-color-mode="dark">
-            <div className="wmde-markdown-var">
-                <button onClick={handleClickSaveBlog}>
-                    save
-                </button>
-            </div>
-            <MDEditor value={value} onChange={() => setValue} height={1200} />
-        </section>
+      <section data-color-mode="dark">
+        <div className="wmde-markdown-var">
+          <button onClick={handleClickSaveBlog}>save</button>
+        </div>
+        <div className="px-6">
+          <Editor
+            initialValue={value}
+            previewStyle="vertical"
+            height="1200px"
+            initialEditType="markdown"
+            useCommandShortcut={true}
+            ref={editorRef}
+          />
+        </div>
+      </section>
     </main>
   )
 }
