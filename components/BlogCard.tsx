808f7550b72bb8cab16984b51756dc5ab9a6e058
Bill1907 <a31829660@gamil.com>
2023-12-07 23:31:49 +0900

    💄 blog card list style

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -1,9 +1,33 @@
-import { Blog } from '@/types/blog'
+import { Blog } from '@/types/blog';
 
 interface BlogCardProps {
-  blog: Blog
+  blog: Blog;
 }
 
-export default async function BlogCard({ blog }: BlogCardProps) {
-  return <div className="w-full min-h-30 relative">{blog.title}</div>
+export default function BlogCard({ blog }: BlogCardProps) {
+  const fotmatDate = (date: Date) => {
+    const d = new Date(date);
+    return `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
+  };
+
+  return (
+    <div className="w-full min-h-30 relative flex flex-col gap-6 my-4">
+      <div className="">
+        <span className="text-xl font-medium">{blog.title}</span>
+      </div>
+      <div className="flex justify-between">
+        <div>
+          {blog.tags.map((tag, index) => (
+            <span
+              className="bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 cursor-pointer hover:bg-gray-400"
+              key={`tag_${index}`}
+            >
+              #{tag}
+            </span>
+          ))}
+        </div>
+        {blog.updatedAt && <span>{fotmatDate(blog.updatedAt)}</span>}
+      </div>
+    </div>
+  );
 }
