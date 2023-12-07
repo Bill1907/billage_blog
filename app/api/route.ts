d600743d4f414c9bbf2de271fb59c6d3d3f63397
Bill1907 <a31829660@gamil.com>
2023-12-07 22:36:53 +0900

    create blog

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,24 +1,11 @@
-import { connectDB } from '@/util/database'
+import { connectDB } from '@/util/database';
 
 export async function GET() {
   try {
-    const db = (await connectDB).db('test')
-    const blogs = await db.collection('blogs').find().toArray()
-    return Response.json(blogs)
+    const db = (await connectDB).db('test');
+    const blogs = await db.collection('blogs').find().toArray();
+    return Response.json(blogs);
   } catch (e) {
-    console.log(e)
-  }
-}
-
-export async function POST() {
-  try {
-    const db = (await connectDB).db('test')
-    const result = await db.collection('blogs').insertOne({
-      title: 'Hello World',
-      content: 'This is my first blog',
-    })
-    return Response.json(result)
-  } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 }
