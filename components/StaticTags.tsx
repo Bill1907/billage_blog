263fdb89c35936c27158d79aee66e036cbb2c824
Boseong <a31829660@gmail.com>
2023-12-14 22:49:18 +0900

    🧹detail page api (#4)
    Co-authored-by: Bill1907 <a31829660@gamil.com>

--- components/StaticTags.tsx
+++ components/StaticTags.tsx
@@ -6,9 +6,9 @@ const fetchStaticTags = async () => {
 export default async function StaticTags() {
   const staticTags = await fetchStaticTags();
   return (
-    <div className="max-w-5xl flex flex-row flex-wrap gap-4">
+    <div className="max-w-5xl flex flex-row flex-wrap gap-4 select-none">
       {staticTags.map((tagInfo: { name: string; count: number }) => (
-        <div className="py-2 px-4 bg-gradient-to-r from-gray-500 to-gray-800 rounded-2xl flex flex-wrap gap-2">
+        <div className="py-2 px-4 bg-gradient-to-r from-gray-500 to-gray-800 rounded-2xl flex flex-wrap gap-2 cursor-pointer ">
           <span className="text-lg"># {tagInfo.name}</span>
         </div>
       ))}
